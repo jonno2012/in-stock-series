9be172d43b8a0f25511f531c13e2f8c60bda672c
jonno2012 <jonathanrhill78@gmail.com>
2023-03-02 08:19:54 +0000

    Mockery, anonymous classes, testing

--- tests/Feature/StockTest.php
+++ tests/Feature/StockTest.php
@@ -3,11 +3,14 @@
 namespace Tests\Feature;
 
 use App\Clients\ClientException;
+use Facades\App\Clients\ClientFactory;
+use App\Clients\StockStatus;
 use Database\Seeders\RetailerWithProductSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\Retailer;
 use App\Models\Stock;
+use App\Clients\Client;
 
 class StockTest extends TestCase
 {
@@ -23,7 +26,31 @@ class StockTest extends TestCase
         $this->expectException(ClientException::class);
 
         Stock::first()->track();
+    }
 
+    /** @test  */
+    public function it_updates_local_stock_status_after_being_tracked()
+    {
+        $this->seed(RetailerWithProductSeeder::class);
 
+        $clientMock = \Mockery::mock(Client::class);
+        $clientMock->shouldReceive('checkAvailability')->andReturn(new StockStatus($available = true, $price = 9900));
+
+        ClientFactory::shouldReceive('make->checkAvailability')->andReturn(
+            new StockStatus($available = true, $price = 9900)
+        );
+
+//        ClientFactory::shouldReceive('make')->andReturn(new class implements Client // anonymous class
+//        {
+//            public function checkAvailability(Stock $stock): StockStatus
+//            {
+//                return new StockStatus($available = true, $price = 9900); // leave variables so we remember what they are.
+//            }
+//        });
+
+        $stock = tap(Stock::first())->track(); //Call the given Closure with the given value then return the value.
+
+        $this->assertTrue($stock->in_stock);
+        $this->assertEquals(9900, $stock->price);
     }
 }
